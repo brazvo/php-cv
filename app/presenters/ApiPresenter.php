1101b4ead54a54cdb9f50f0ffb4c58b1543941dc
Branislav Zvolensky <branislav.zvolensky@mb-e.sk>
2015-12-12 00:10:53 +0100

    buttons to about page

--- app/presenters/ApiPresenter.php
+++ app/presenters/ApiPresenter.php
@@ -21,7 +21,7 @@ class ApiPresenter extends Presenter
         if( file_exists(APP_DIR . '/data/en/cv.json') ) {
             $data = json_decode( file_get_contents(APP_DIR . '/data/en/cv.json'), true );
         }
-        
+
         header('Access-Control-Allow-Origin: *');
 
         $this->sendResponse(new JsonResponse($data));
